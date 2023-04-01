20ebab89c77eb2678fa56491777b8ca1b5c4cce4
Ayse Arzu <aysebattal@gmail.com>
2023-04-01 16:38:10 +0300

    day08

--- src/test/java/bireysel/Task03_ForJUnit.java
+++ src/test/java/bireysel/Task03_ForJUnit.java
@@ -21,9 +21,6 @@ public class Task03_ForJUnit {
     -AfterClass ile kapatın
      */
     static WebDriver driver;
-    static String rakam1;
-    static String rakam2;
-    static String rakam3;
 
     @BeforeClass
     public static void start() {
@@ -43,9 +40,9 @@ public class Task03_ForJUnit {
 
     @After
     public void tearDown() {
-        System.out.println("The God Father adinda " + rakam1 + " sonuc bulundu");
-        System.out.println("Lord of the Rings " + rakam2 + " sonuc bulundu");
-        System.out.println("Kill Bill " + rakam3 + " sonuc bulundu");
+
+        String[] sonuc = driver.findElement(By.xpath("//*[@id='result-stats']")).getText().split(" ");
+        System.out.println("Arama Sonuc Sayısı = " + sonuc[1]);
     }
 
     @AfterClass
@@ -56,34 +53,28 @@ public class Task03_ForJUnit {
 
     }
 
+    // -arama kutusuna "The God Father" yazip, cikan sonuc sayisini yazdirin
     @Test
-    public void test01() {
-        // -arama kutusuna "The God Father" yazip, cikan sonuc sayisini yazdirin
+    public void test01() throws InterruptedException {
+
         driver.findElement(By.xpath("//div[text()='Alles accepteren']")).click();
         driver.findElement(By.xpath("//input[@class='gLFyf']")).sendKeys("Lord of the Rings" + Keys.ENTER);
-        String sonuc = driver.findElement(By.xpath("//div[@id='result-stats']")).getText();
-        rakam1 = sonuc.split(" ")[1];
-
 
     }
 
+    //-arama kutusuna  "Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
     @Test
-    public void test02() {
-        //-arama kutusuna  "Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
-        driver.findElement(By.xpath("//div[text()='Alles accepteren']")).click();
+    public void test02() throws InterruptedException {
+
         driver.findElement(By.xpath("//input[@class='gLFyf']")).sendKeys("The God Father" + Keys.ENTER);
-        String sonuc = driver.findElement(By.xpath("//div[@id='result-stats']")).getText();
-        rakam2 = sonuc.split(" ")[1];
 
     }
 
+    //-arama kutusuna  "Kill Bill" yazip, cikan sonuc sayisini yazdirin
     @Test
-    public void test03() {
-        //-arama kutusuna  "Kill Bill" yazip, cikan sonuc sayisini yazdirin
-        driver.findElement(By.xpath("//div[text()='Alles accepteren']")).click();
+    public void test03() throws InterruptedException {
+
         driver.findElement(By.xpath("//input[@class='gLFyf']")).sendKeys("Kill Bill" + Keys.ENTER);
-        String sonuc = driver.findElement(By.xpath("//div[@id='result-stats']")).getText();
-        rakam3 = sonuc.split(" ")[1];
 
     }
 }
---
+++ src/test/java/bireysel/Task04_ForJUnit.java
@@ -0,0 +1,65 @@
+package bireysel;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Task04_ForJUnit {
+    /*
+        -Amazon sayfasına gidelim
+        -Arama Kutusundaki Dropdown menuyu yazdıralım
+        -Dropdown menuden sırasıyla ilk 5 başlığı (Arts&Crafts ile başlayıp Books'a kadar Books dahil) seçip
+        başlık altındakileri aratalım. Her aramada sayfa başlığını yazdıralım
+        Not: Select Class'ı kullanalım
+         */
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        driver.close();
+    }
+
+    @Test
+    public void dropDown() {
+        // -Amazon sayfasına gidelim
+        driver.get("https://amazon.com");
+
+        // -Arama Kutusundaki Dropdown menuyu yazdıralım
+        WebElement dropBox = driver.findElement(By.xpath("//select[@aria-describedby='searchDropdownDescription']"));
+
+        Select select = new Select(dropBox);
+        List<WebElement> liste = select.getOptions();
+        liste.stream().skip(1).forEach(t -> System.out.println(t.getText()));
+
+        // -Dropdown menuden sırasıyla ilk 5 başlığı (Arts&Crafts ile başlayıp Books'a kadar Books dahil) seçip
+        //  başlık altındakileri aratalım. Her aramada sayfa başlığını yazdıralım
+
+        for (int i = 1; i <= 6; i++) {
+            WebElement dropDown = driver.findElement(By.cssSelector("select[aria-describedby='searchDropdownDescription']"));
+            Select select1 = new Select(dropDown);
+            select1.selectByIndex(i);
+            driver.findElement(By.id("nav-search-submit-button")).click();
+            System.out.println(i + ". sayfa basligi: " + driver.getTitle());
+            driver.navigate().back();
+        }
+    }
+}
